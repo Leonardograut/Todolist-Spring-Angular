ce144354389d73e122abd3141ef007bb73b07137
Leonardograut <omgleo@live.com>
2023-06-25 21:33:56 -0300

    Projeto Criado

---
+++ src/main/java/com/projeto/backend/model/Cliente.java
@@ -0,0 +1,92 @@
+package com.projeto.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long  id;
+
+
+
+    private String nome;
+
+
+    private int idade;
+
+    private String email;
+
+    private String ciddade;
+
+
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCiddade() {
+        return ciddade;
+    }
+
+    public void setCiddade(String ciddade) {
+        this.ciddade = ciddade;
+    }
+
+
+    public Cliente(){
+
+
+    }
+
+    
+
+    
+    public Cliente(long id, String nome, int idade, String email, String ciddade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.ciddade = ciddade;
+    }
+
+
+
+}
